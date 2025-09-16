34c7589c09c4ba5ff34a4d1a5bc5882db059e1ab
Gabs <cardosogabrielle601@gmail.com>
2025-09-15 23:08:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿for(int contador = 5; contador <= 20; contador++)
+{
+    Console.WriteLine("Valor do contador é " + contador);
+}
+Console.WriteLine("fim");
+
+
+//testes de códigos utilizando for 
